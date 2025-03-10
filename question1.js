b93c51fa08e76e3a4685401d898aaab29c2ab6f9
unknown <YY110@ccs.local>
2025-03-10 17:21:22 +0800

    Add question1. JavaScript Fundamentals

---
+++ question1.js
@@ -0,0 +1,7 @@
+// Function to reverse a string
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Test the function
+console.log(reverseString("hello")); // Output: "olleh"
